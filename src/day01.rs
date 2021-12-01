0e84ff9fb14d79368089cc47c2e0316a3a83e0b3
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2021-12-01 10:59:06 +0100

    [ENH] improve day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,26 +1,21 @@
-// #[aoc(day01)]
-// pub fn generator(input: &str) -> Vec<u32> {
-//     input.lines().map(|x| x.parse().unwrap()).collect()
-// }
-// 
-// #[aoc(day01, part1)]
-pub fn part1(input: Vec<u32>)-> u32 {
+pub fn parse_input(input: &str) -> Vec<u32> {
+    input.lines().map(|x| x.parse().unwrap()).collect()
+}
+
+pub fn part1(input: &Vec<u32>) -> u32 {
     let mut total = 0;
-    let i1 = &input.clone()[..];
-    let i2 = &input.clone()[1..];
-    for (x, y) in i1.iter().zip(i2.iter()) {
+    for (x, y) in input.iter().zip(input[1..].iter()) {
         if x < y {
             total += 1;
         }
     }
-    println!("{}", total);
     total
 }
 
-pub fn part2(input: Vec<u32>) -> u32 {
+pub fn part2(input: &Vec<u32>) -> u32 {
     let mut strides: Vec<u32> = Vec::new();
-    for i in 0..input.len()-2 {
-        strides.push(input[i..=i+2].iter().sum());
+    for i in 0..input.len() - 2 {
+        strides.push(input[i..=i + 2].iter().sum());
     }
-    part1(strides)
+    part1(&strides)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ use std::fs;
 
 fn main() {
     let input: String = fs::read_to_string("input/2021/day01.txt").unwrap();
-    let parsed: Vec<u32> = input
-        .lines()
-        .map(|l| {
-            l.parse().unwrap()
-        })
-    .collect();
-    day01::part1(parsed.clone());
-    day01::part2(parsed);
+    let parsed = day01::parse_input(&input);
+    let part1 = day01::part1(&parsed);
+    let part2 = day01::part2(&parsed);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
