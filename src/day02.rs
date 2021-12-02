7b85ba96354b59e26f10f1bbdcd11c3f505d53c7
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2021-12-02 12:21:11 +0100

    day02 improve ref

--- src/day02.rs
+++ src/day02.rs
@@ -13,8 +13,8 @@ pub fn parse_input(input: &str) -> Vec<(&str, u32)> {
 pub fn part1(input: &Vec<(&str, u32)>) -> u32 {
     let mut total = 0;
     let mut depth = 0;
-    for (x, y) in input {
-        match x.as_ref() {
+    for &(x, y) in input {
+        match x {
             "forward" => total += y,
             "down" => depth += y,
             _ => depth -= y,
@@ -27,8 +27,8 @@ pub fn part2(input: &Vec<(&str, u32)>) -> u32 {
     let mut aim = 0;
     let mut total = 0;
     let mut depth = 0;
-    for (x, y) in input {
-        match x.as_ref() {
+    for &(x, y) in input {
+        match x {
             "forward" => {
                 total += y;
                 depth += aim * y
