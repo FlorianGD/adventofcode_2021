cee3ed6c607dcc24464c2713b0324af794d803d6
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2021-12-02 09:32:11 +0100

    day02 refactor to use match

--- src/day02.rs
+++ src/day02.rs
@@ -1,41 +1,41 @@
-pub fn parse_input(input: &str) -> Vec<(String,u32)> {
-    input.lines().map(|x| {
-        let mut y = x.split_whitespace();
-        let action = y.next().unwrap().to_string();
-        let direction: u32 = y.next().unwrap().parse().unwrap();
-        (action, direction)
-    }).collect()
+pub fn parse_input(input: &str) -> Vec<(&str, u32)> {
+    input
+        .lines()
+        .map(|x| {
+            let mut y = x.split_whitespace();
+            let action = y.next().unwrap();
+            let direction: u32 = y.next().unwrap().parse().unwrap();
+            (action, direction)
+        })
+        .collect()
 }
 
-pub fn part2(input: &Vec<(String, u32)>) -> u32 {
-    let mut aim = 0;
+pub fn part1(input: &Vec<(&str, u32)>) -> u32 {
     let mut total = 0;
-    let mut depth =0;
-    for (x, y) in input.iter() {
-        if x == "forward" {
-            total += y;
-            depth += aim * y
-        } else if x == "down" {
-            aim += y
-        } else {
-            aim -= y
-        }
-    }
-    total * depth 
-}
-
-pub fn part1(input: &Vec<(String, u32)>) -> u32 {
-    let mut total = 0;
-    let mut depth =0;
-    for (x, y) in input.iter() {
-        if x == "forward" {
-            total += y
-        } else if x == "down" {
-            depth += y
-        } else {
-            depth -= y
+    let mut depth = 0;
+    for (x, y) in input {
+        match x.as_ref() {
+            "forward" => total += y,
+            "down" => depth += y,
+            _ => depth -= y,
         }
     }
     total * depth
 }
 
+pub fn part2(input: &Vec<(&str, u32)>) -> u32 {
+    let mut aim = 0;
+    let mut total = 0;
+    let mut depth = 0;
+    for (x, y) in input {
+        match x.as_ref() {
+            "forward" => {
+                total += y;
+                depth += aim * y
+            }
+            "down" => aim += y,
+            _ => aim -= y,
+        }
+    }
+    total * depth
+}
