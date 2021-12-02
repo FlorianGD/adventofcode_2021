ea175c32854983774b5f55bb538d8842df7f7780
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2021-12-02 06:56:27 +0100

    day02 part 1

---
+++ src/day02.rs
@@ -0,0 +1,27 @@
+pub fn parse_input(input: &str) -> Vec<(String,u32)> {
+    input.lines().map(|x| {
+        let mut y = x.split_whitespace();
+        let action = y.next().unwrap().to_string();
+        let direction: u32 = y.next().unwrap().parse().unwrap();
+        (action, direction)
+    }).collect()
+}
+
+pub fn part1(input: &Vec<(String, u32)>) -> u32 {
+    let mut total = 0;
+    let mut depth =0;
+    for (x, y) in input.iter() {
+        if x == "forward" {
+            total += y
+        } else if x == "down" {
+            depth += y
+        } else {
+            depth -= y
+        }
+    }
+    total * depth
+}
+
+pub fn part2(input: &Vec<(&str, u32)>) -> u32 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use adventofcode_2021::day01;
+use adventofcode_2021::day02;
 use std::fs;
 
 fn main() {
-    let input: String = fs::read_to_string("input/2021/day01.txt").unwrap();
-    let parsed = day01::parse_input(&input);
-    let part1 = day01::part1(&parsed);
-    let part2 = day01::part2(&parsed);
+    let test="\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+";
+    let input: String = fs::read_to_string("input/2021/day02.txt").unwrap();
+    let parsed = day02::parse_input(&test);
+    //let parsed = day02::parse_input(&input);
+    let part1 = day02::part1(&parsed);
+//    let part2 = day02::part2(&parsed);
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
 }
