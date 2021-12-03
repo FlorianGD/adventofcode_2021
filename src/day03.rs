b6e0de1d5de5ce5d3d9e68caea69ea02c2b49ec9
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2021-12-03 09:02:35 +0100

    day03 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,18 +1,13 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
-pub fn parse_input(input: &str) -> Vec<Vec<char>> {
+pub fn parse_input(input: &str) -> Vec<Vec<u32>> {
     input
         .lines()
-        .map(|x| {
-            x.chars().collect()
-            // let mut y = x.split_whitespace();
-            // let action = y.next().unwrap();
-            // let direction: u32 = y.next().unwrap().parse().unwrap();
-            // (action, direction)
-        })
+        .map(|x| x.chars().map(|y| y.to_digit(10).unwrap()).collect())
         .collect()
 }
 
+// from stackoverflow
 fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
 where
     T: Clone,
@@ -23,23 +18,40 @@ where
         .collect()
 }
 
-pub fn part1(input: &Vec<Vec<char>>) -> u32 {
-    let positions = transpose(input.clone());
-    let counts: Vec<_> = positions.iter().map(|bits| {
-        bits.iter().fold(HashMap::new(), |mut hm, bit| {
-           *hm.entry(bit).or_insert(0) += 1;
-           hm
-        })
-    }).collect();
-    let maxs: Vec<_> = counts.iter().map(|hm| {
-        hm.iter()
-            .max_by(|(_k1, v1), (_k2, v2)| v1.cmp(&v2))
-            .map(|(k, _v)| k)
-            .expect("huho")
-    }).collect();
+fn vec_to_int(vals: Vec<u8>) -> u32 {
+    vals.iter().rev().enumerate().fold(0, |acc, (idx, val)| {
+        acc + 2_u32.pow(idx.try_into().unwrap()) * (*val as u32)
+    })
+}
 
-    println!("{:?}", maxs); 
-    0
+pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
+    let positions = transpose(input.clone());
+    let counts: Vec<_> = positions
+        .iter()
+        .map(|bits| {
+            bits.iter().fold(HashMap::new(), |mut hm, bit| {
+                *hm.entry(bit).or_insert(0) += 1;
+                hm
+            })
+        })
+        .collect();
+    let maxs: Vec<u8> = counts
+        .iter()
+        .map(|hm| {
+            hm.iter()
+                .max_by(|&(_k1, v1), &(_k2, v2)| v1.cmp(&v2))
+                .map(|(&k, _v)| *k as u8)
+                .expect("huho")
+        })
+        .collect();
+
+    let mins: Vec<u8> = maxs.iter().map(|x| 1 - x).collect();
+    let maxs = vec_to_int(maxs);
+    let mins = vec_to_int(mins);
+
+    println!("{:?}", maxs);
+    println!("{:?}", mins);
+    maxs * mins
 }
 
 // pub fn part2(input: &Vec<(&str, u32)>) -> u32 {
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@ use adventofcode_2021::day03;
 use std::fs;
 
 fn main() {
-//    let input: String = fs::read_to_string("input/2021/day03.txt").unwrap();
-    let input = "\
-00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010";
+    let input: String = fs::read_to_string("input/2021/day03.txt").unwrap();
+    //     let input = "\
+    // 00100
+    // 11110
+    // 10110
+    // 10111
+    // 10101
+    // 01111
+    // 00111
+    // 11100
+    // 10000
+    // 11001
+    // 00010
+    // 01010";
     let parsed = day03::parse_input(&input);
     let part1 = day03::part1(&parsed);
     //let part2 = day03::part2(&parsed);
