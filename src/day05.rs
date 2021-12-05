b6310c12f96de68353d67d6a0d7280eb0d4770f1
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2021-12-05 23:09:10 +0100

    day05 WIP

---
+++ src/day05.rs
@@ -0,0 +1,61 @@
+use std::{collections::HashMap, num::ParseIntError, str::FromStr};
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Point {
+    x: u32,
+    y: u32,
+}
+
+impl FromStr for Point {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coords: Vec<&str> = s.split(',').collect();
+
+        let x_fromstr = coords[0].parse::<u32>()?;
+        let y_fromstr = coords[1].parse::<u32>()?;
+
+        Ok(Point {
+            x: x_fromstr,
+            y: y_fromstr,
+        })
+    }
+}
+
+pub fn parse_input(input: &str) -> Vec<(Point, Point)> {
+    input
+        .lines()
+        .map(|line| {
+            let (a, b) = line.split_once(" -> ").unwrap();
+            (a.parse().unwrap(), b.parse().unwrap())
+        })
+        .collect()
+}
+
+pub fn part1(input: &Vec<(Point, Point)>) -> usize {
+    let mut map = HashMap::new();
+    let horizontal_lines: Vec<&(Point, Point)> =
+        input.iter().filter(|&(p1, p2)| p1.x == p2.x).collect();
+    println!("{:?}", horizontal_lines);
+    for (p1, p2) in horizontal_lines {
+        let y_min = (p1.min(p2)).y;
+        let y_max = (p1.max(p2)).y;
+        let x = p1.x;
+        // println!("x={}, y min={}, y max= {}", x, y_min, y_max);
+        for y in y_min..y_max {
+            *map.entry(Point { x, y }).or_insert(0) += 1;
+        }
+    }
+    println!("{:?}", map);
+
+    let vertical_lines = input.iter().filter(|&(p1, p2)| p1.y == p2.y);
+    for (p1, p2) in vertical_lines {
+        let x_min = (p1.min(p2)).x;
+        let x_max = (p1.max(p2)).x;
+        let y = p1.y;
+        for x in x_min..x_max {
+            *map.entry(Point { x, y }).or_insert(0) += 1;
+        }
+    }
+    println!("{:?}", map);
+    map.into_values().filter(|&v| v >= 2).count()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
-use adventofcode_2021::day04;
+use adventofcode_2021::day05;
 use std::{fs, time::Instant};
 
 fn main() {
-    let input: String = fs::read_to_string("input/2021/day04.txt").unwrap();
-    let mut now = Instant::now();
-    let parsed = day04::parse_input(&input);
-    let mut elapsed_time = now.elapsed();
-    println!("\t⏱️ parsing took {} µs.", elapsed_time.as_micros());
+    let input = "\
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+";
+    // let input: String = fs::read_to_string("input/2021/day04.txt").unwrap();
+    // let mut now = Instant::now();
+    let parsed = day05::parse_input(&input);
+    // println!("{:?}", parsed);
+    // let mut elapsed_time = now.elapsed();
+    // println!("\t⏱️ parsing took {} µs.", elapsed_time.as_micros());
 
-    now = Instant::now();
-    let part1 = day04::part1(&parsed);
+    // now = Instant::now();
+    let part1 = day05::part1(&parsed);
     println!("Part 1: {:#?}", part1);
-    elapsed_time = now.elapsed();
-    println!("\t⏱️ part 1 took {} µs.", elapsed_time.as_micros());
+    // elapsed_time = now.elapsed();
+    // println!("\t⏱️ part 1 took {} µs.", elapsed_time.as_micros());
 
-    now = Instant::now();
-    let part2 = day04::part2(&parsed);
-    println!("Part 2: {:?}", part2);
-    elapsed_time = now.elapsed();
-    println!("\t⏱️ part 2 took {} µs.", elapsed_time.as_micros());
+    // now = Instant::now();
+    // let part2 = day04::part2(&parsed);
+    // println!("Part 2: {:?}", part2);
+    // elapsed_time = now.elapsed();
+    // println!("\t⏱️ part 2 took {} µs.", elapsed_time.as_micros());
 }
