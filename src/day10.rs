2cf6eb655d8d3da601bc36942d9891f8a565d9b5
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2021-12-10 14:45:51 +0100

    day10 part 1

--- src/day10.rs
+++ src/day10.rs
@@ -2,38 +2,51 @@ use std::collections::HashMap;
 
 #[aoc_generator(day10)]
 pub fn parse_input(input: &str) -> Vec<String> {
-    let input ="\
-[({(<(())[]>[[{[]{<()<>>
-[(()[<>])]({[<{<<[]>>(
-{([(<{}[<>[]}>{[]{[(<()>
-(((({<>}<{<{<>}{[]{[]{}
-[[<[([]))<([[{}[[()]]]
-[{[{({}]{}}([{[{{{}}([]
-{<[[]]>}<{[{[{[]{()[[[]
-[<(<(<(<{}))><([]([]()
-<{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]
-";
+    //     let input = "\
+    // [({(<(())[]>[[{[]{<()<>>
+    // [(()[<>])]({[<{<<[]>>(
+    // {([(<{}[<>[]}>{[]{[(<()>
+    // (((({<>}<{<{<>}{[]{[]{}
+    // [[<[([]))<([[{}[[()]]]
+    // [{[{({}]{}}([{[{{{}}([]
+    // {<[[]]>}<{[{[{[]{()[[[]
+    // [<(<(<(<{}))><([]([]()
+    // <{([([[(<>()){}]>(<<{{
+    // <{([{{}}[<[[[<>{}]]]>[]]
+    // ";
     input.lines().map(|x| x.to_string()).collect()
 }
 
 #[aoc(day10, part1)]
-pub fn part1(lines : &Vec<String>) -> u32 {
+pub fn part1(lines: &Vec<String>) -> u32 {
+    let mut score = 0;
+    let points = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+    let closing_to_opening = HashMap::from([(')', '('), (']', '['), ('}', '{'), ('>', '<')]);
     for line in lines {
-        let mut parens = HashMap::new();
-        for (i, c) in line.chars().enumerate() {
+        let mut scope = Vec::new();
+        for c in line.chars() {
             match c {
-                '{' | '(' | '[' | '<' => *parens.entry(c).or_insert(0) +=1,
-                '}' => *parens.entry('{').or_insert(0) -=1,
-                ')' => *parens.entry('(').or_insert(0) -=1,
-                ']' => *parens.entry('[').or_insert(0) -=1,
-                '>' => *parens.entry('<').or_insert(0) -=1,
+                '{' | '(' | '[' | '<' => {
+                    scope.push(c);
+                }
+                '}' | ')' | ']' | '>' => {
+                    let current = scope.pop();
+                    match current {
+                        Some(v) => {
+                            if closing_to_opening[&c] != v {
+                                score += points[&c];
+                                break;
+                            }
+                        }
+                        None => {
+                            score += points[&c];
+                            break;
+                        }
+                    }
+                }
                 _ => unreachable!(),
             }
         }
-
-        println!("{:?}", parens);
     }
-    0
+    score
 }
-
