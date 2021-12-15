14a825c254109ce2b05fd6fb7e7f5d761fa2f7b1
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2021-12-15 09:14:35 +0100

    day14 part 1 and 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use itertools::Itertools;
+use std::collections::HashMap;
 type Pair = (char, char);
 #[aoc_generator(day14)]
 pub fn parse_input(input: &str) -> (Vec<char>, HashMap<Pair, (Pair, Pair)>) {
@@ -7,46 +7,67 @@ pub fn parse_input(input: &str) -> (Vec<char>, HashMap<Pair, (Pair, Pair)>) {
     let pairs = pairs
         .lines()
         .map(|l| l.split_once(" -> ").unwrap())
-        .map(|(a,b)| (a.chars().next_tuple().unwrap(), b.chars().next().unwrap()))
-        .map(|((a,b), c)| ((a,b), ((a,c), (c,b))))
+        .map(|(a, b)| (a.chars().next_tuple().unwrap(), b.chars().next().unwrap()))
+        .map(|((a, b), c)| ((a, b), ((a, c), (c, b))))
         .collect();
     (template.chars().collect(), pairs)
 }
 
-fn step(counts: &HashMap<Pair, usize>, pairs: &HashMap<Pair, (Pair, Pair)>) -> HashMap<Pair, usize> {
+fn step(
+    counts: &HashMap<Pair, usize>,
+    pairs: &HashMap<Pair, (Pair, Pair)>,
+) -> HashMap<Pair, usize> {
     let mut new_counts = counts.clone();
     for (p, v) in counts {
-        let (p1,p2) = pairs[&p];
+        let (p1, p2) = pairs[&p];
         *new_counts.entry(p1).or_insert(0) += v;
         *new_counts.entry(p2).or_insert(0) += v;
         *new_counts.entry(*p).or_insert(0) -= v;
     }
     new_counts
-
 }
 
 fn score(counts: &HashMap<Pair, usize>, first: &char, last: &char) -> usize {
     let mut letters = HashMap::new();
-    for ((a,b),v) in counts {
-        *letters.entry(a).or_insert(0) +=v;
-        *letters.entry(b).or_insert(0) +=v;
+    for ((a, b), v) in counts {
+        *letters.entry(a).or_insert(0) += v;
+        *letters.entry(b).or_insert(0) += v;
     }
-    *letters.entry(first).or_insert(0) +=1;
-    *letters.entry(last).or_insert(0)  +=1;
-    println!("{:?}", letters);
-    letters.values().max().unwrap() - letters.values().min().unwrap()
+    *letters.entry(first).or_insert(0) += 1;
+    *letters.entry(last).or_insert(0) += 1;
+    letters.values().max().unwrap() - letters.values().min().unwrap() / 2
 }
 
 #[aoc(day14, part1)]
 pub fn part1((template, pairs): &(Vec<char>, HashMap<Pair, (Pair, Pair)>)) -> usize {
     let mut counts = HashMap::new();
     for p in template.windows(2) {
-        *counts.entry((p[0],p[1])).or_insert(0) +=1;
+        *counts.entry((p[0], p[1])).or_insert(0) += 1;
+    }
+    let mut new_counts = counts.clone();
+    for _ in 0..10 {
+        new_counts = step(&new_counts, pairs);
+    }
+    score(
+        &new_counts,
+        template.first().unwrap(),
+        template.last().unwrap(),
+    )
+}
+
+#[aoc(day14, part2)]
+pub fn part2((template, pairs): &(Vec<char>, HashMap<Pair, (Pair, Pair)>)) -> usize {
+    let mut counts = HashMap::new();
+    for p in template.windows(2) {
+        *counts.entry((p[0], p[1])).or_insert(0) += 1;
     }
     let mut new_counts = counts.clone();
     for _ in 0..40 {
         new_counts = step(&new_counts, pairs);
     }
-    score(&new_counts, template.first().unwrap(), template.last().unwrap()) / 2
+    score(
+        &new_counts,
+        template.first().unwrap(),
+        template.last().unwrap(),
+    )
 }
-
